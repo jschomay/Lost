da311dff5aadf9356f581842f4d7a90589088713
jschomay <23263504-jschomay@users.noreply.replit.com>
2023-05-22 22:54:43 +0000

    sort of zoom and move

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ from sprites import Background
 pygame.init()
 clock = pygame.time.Clock()
 
+background_color = (40, 40, 40)
 screen = pygame.display.set_mode((400,300))
 pygame.display.set_caption('Lost')
+screen.fill(background_color)
 
 pygame.display.update()
 
-gameExit = False
+game_exit = False
 
 background = Background("images/background1.jpg", screen)
 
-while not gameExit:
+while not game_exit:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             gameExit = True
     
     background.handle_events(events)
-    background.update()
+    background.update(screen)
     background.draw(screen)
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
--- sprites.py
+++ sprites.py
@@ -2,21 +2,44 @@ import pygame
 
 
 class Background():
-    SCALE_FACTOR = 3
-    SPEED = 5
+    INITIAL_SCALE_FACTOR = 3
+    SPEED = 3
+    ZOOM_EFFECT = 1
 
     def __init__(self, background, screen):
-        self.image = pygame.image.load(background).convert()
-        new_size = (screen.get_width() * self.SCALE_FACTOR,
-                    screen.get_height() * self.SCALE_FACTOR)
-        self.image = pygame.transform.scale(self.image, new_size)
-        self.position = [0, 0]
+        self.original_image = pygame.image.load(background).convert()
+        new_size = (screen.get_width() * self.INITIAL_SCALE_FACTOR,
+                    screen.get_height() * self.INITIAL_SCALE_FACTOR)
+        self.image = pygame.transform.scale(self.original_image, new_size)
+
+        self.position = [(screen.get_width() - self.image.get_width()) / 2,
+                         (screen.get_height() - self.image.get_height())]
         self.speed_x = 0
         self.speed_y = 0
 
-    def update(self):
+    def update(self, screen):
         self.position[0] += self.speed_x
         self.position[1] += self.speed_y
+
+        if self.position[1] > 0: self.position[1] = 0
+        if self.position[0] > 0: self.position[0] = 0
+        if self.position[1] < screen.get_height() - self.image.get_height():
+            self.position[1] = screen.get_height() - self.image.get_height()
+        if self.position[0] < screen.get_width() - self.image.get_width():
+            self.position[0] = screen.get_width() - self.image.get_width()
+
+        old_size = self.image.get_size()
+        center = self.position[1] + self.image.get_height() / 2
+        zoom_factor = (1 + (center - screen.get_height() / 2) /
+                       screen.get_height()) * self.ZOOM_EFFECT
+        new_size = (int(self.original_image.get_width() * zoom_factor),
+                    int(self.original_image.get_height() * zoom_factor))
+
+        self.position[0] -= (new_size[0] - old_size[0]) / 2
+        self.position[1] -= (new_size[1] - old_size[1]) / 2
+
+        self.image = pygame.transform.scale(self.original_image, new_size)
+
 
     def draw(self, surface):
         surface.blit(self.image, self.position)
