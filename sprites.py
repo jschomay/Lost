686d0f94fbc3849c836d37a68a36c200b4891306
jschomay <23263504-jschomay@users.noreply.replit.com>
2023-05-23 00:48:11 +0000

    get zoom working well

--- sprites.py
+++ sprites.py
@@ -2,9 +2,9 @@ import pygame
 
 
 class Background():
-    INITIAL_SCALE_FACTOR = 3
-    SPEED = 3
-    ZOOM_EFFECT = 1
+    INITIAL_SCALE_FACTOR = 2
+    SPEED = 2.5
+    ZOOM_SPEED = 0.002
 
     def __init__(self, background, screen):
         self.original_image = pygame.image.load(background).convert()
@@ -16,30 +16,33 @@ class Background():
                          (screen.get_height() - self.image.get_height())]
         self.speed_x = 0
         self.speed_y = 0
+        self.y = 0
 
     def update(self, screen):
         self.position[0] += self.speed_x
         self.position[1] += self.speed_y
-
-        if self.position[1] > 0: self.position[1] = 0
+            
         if self.position[0] > 0: self.position[0] = 0
-        if self.position[1] < screen.get_height() - self.image.get_height():
-            self.position[1] = screen.get_height() - self.image.get_height()
         if self.position[0] < screen.get_width() - self.image.get_width():
             self.position[0] = screen.get_width() - self.image.get_width()
 
+        if self.position[1] > 0: 
+            self.position[1] = 0
+        elif self.position[1] < screen.get_height() - self.image.get_height():
+            self.position[1] = screen.get_height() - self.image.get_height()
+        else:
+            self.y += self.speed_y
+
+        zoom_factor = 1 + self.y * self.ZOOM_SPEED
+
         old_size = self.image.get_size()
-        center = self.position[1] + self.image.get_height() / 2
-        zoom_factor = (1 + (center - screen.get_height() / 2) /
-                       screen.get_height()) * self.ZOOM_EFFECT
-        new_size = (int(self.original_image.get_width() * zoom_factor),
-                    int(self.original_image.get_height() * zoom_factor))
+        new_size = (int(screen.get_width() * self.INITIAL_SCALE_FACTOR * zoom_factor),
+                    int(screen.get_height() * self.INITIAL_SCALE_FACTOR * zoom_factor))
 
         self.position[0] -= (new_size[0] - old_size[0]) / 2
         self.position[1] -= (new_size[1] - old_size[1]) / 2
 
         self.image = pygame.transform.scale(self.original_image, new_size)
-
 
     def draw(self, surface):
         surface.blit(self.image, self.position)
