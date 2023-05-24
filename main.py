5e25a574a72d9927b8389d451761031e7e3d7b45
jschomay <23263504-jschomay@users.noreply.replit.com>
2023-05-24 13:29:00 +0000

    draw on top half and fix movement

--- main.py
+++ main.py
@@ -4,30 +4,37 @@ from sprites import Background, Vignette
 pygame.init()
 clock = pygame.time.Clock()
 
-background_color = (40, 40, 40)
-screen = pygame.display.set_mode((400,300))
+background_color = (0, 0, 0)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption('Lost')
 screen.fill(background_color)
+
+top_area_size = int(screen.get_height() * 0.6)
+top_area = pygame.Surface((top_area_size, top_area_size))
+top_area_offset = (screen.get_width() // 2 - top_area.get_width() // 2,
+                   screen.get_height() / 20)
+top_area.fill(background_color)
 
 pygame.display.update()
 
 game_exit = False
 
-background = Background("images/background1.jpg", screen)
-vignette = Vignette(screen)
+background = Background("images/background1.jpg", top_area)
+vignette = Vignette(top_area)
 
 while not game_exit:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             gameExit = True
-    
+
     background.handle_events(events)
-    background.update(screen)
-    background.draw(screen)
-    vignette.draw(screen)
+    background.update()
+    background.draw()
+    vignette.draw()
+    screen.blit(top_area, top_area_offset)
     pygame.display.flip()
-    
+
     clock.tick(60)
 
 pygame.quit()
--- sprites.py
+++ sprites.py
@@ -7,6 +7,7 @@ class Background(pygame.sprite.Sprite):
     ZOOM_SPEED = 0.002
 
     def __init__(self, background, screen):
+        self.screen = screen
         pygame.sprite.Sprite.__init__(self)
         self.original_image = pygame.image.load(background).convert()
         new_size = (screen.get_width() * self.INITIAL_SCALE_FACTOR,
@@ -14,32 +15,34 @@ class Background(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.original_image, new_size)
 
         self.position = [(screen.get_width() - self.image.get_width()) / 2,
-                         (screen.get_height() - self.image.get_height())]
+                         (screen.get_height() - self.image.get_height()) / 2]
         self.speed_x = 0
         self.speed_y = 0
         self.y = 0
+        self.key_downs = set()
 
-    def update(self, screen):
-        self.position[0] += self.speed_x
-        self.position[1] += self.speed_y
+    def update(self):
+        self.move()
 
         if self.position[0] > 0: self.position[0] = 0
-        if self.position[0] < screen.get_width() - self.image.get_width():
-            self.position[0] = screen.get_width() - self.image.get_width()
+        if self.position[0] < self.screen.get_width() - self.image.get_width():
+            self.position[0] = self.screen.get_width() - self.image.get_width()
 
         if self.position[1] > 0:
             self.position[1] = 0
-        elif self.position[1] < screen.get_height() - self.image.get_height():
-            self.position[1] = screen.get_height() - self.image.get_height()
+        elif self.position[1] < self.screen.get_height(
+        ) - self.image.get_height():
+            self.position[1] = self.screen.get_height(
+            ) - self.image.get_height()
         else:
             self.y += self.speed_y
 
         zoom_factor = 1 + self.y * self.ZOOM_SPEED
 
         old_size = self.image.get_size()
-        new_size = (int(screen.get_width() * self.INITIAL_SCALE_FACTOR *
+        new_size = (int(self.screen.get_width() * self.INITIAL_SCALE_FACTOR *
                         zoom_factor),
-                    int(screen.get_height() * self.INITIAL_SCALE_FACTOR *
+                    int(self.screen.get_height() * self.INITIAL_SCALE_FACTOR *
                         zoom_factor))
 
         self.position[0] -= (new_size[0] - old_size[0]) / 2
@@ -47,42 +50,53 @@ class Background(pygame.sprite.Sprite):
 
         self.image = pygame.transform.scale(self.original_image, new_size)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.position)
+    def move(self):
+        self.speed_x = 0
+        self.speed_y = 0
+        if pygame.K_UP in self.key_downs:
+            self.speed_y += self.SPEED
+        if pygame.K_DOWN in self.key_downs:
+            self.speed_y += -self.SPEED
+        if pygame.K_LEFT in self.key_downs:
+            self.speed_x += self.SPEED
+        if pygame.K_RIGHT in self.key_downs:
+            self.speed_x += -self.SPEED
+
+        self.position[0] += self.speed_x
+        self.position[1] += self.speed_y
+
+    def draw(self):
+        self.screen.blit(self.image, self.position)
 
     def handle_events(self, event_list):
         for event in event_list:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.speed_y = self.SPEED
-                elif event.key == pygame.K_DOWN:
-                    self.speed_y = -self.SPEED
-                elif event.key == pygame.K_LEFT:
-                    self.speed_x = self.SPEED
-                elif event.key == pygame.K_RIGHT:
-                    self.speed_x = -self.SPEED
+            if event.type == pygame.KEYDOWN and event.key in [
+                    pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT
+            ]:
+                self.key_downs.add(event.key)
 
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    self.speed_y = 0
-                elif event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    self.speed_x = 0
+            elif event.type == pygame.KEYUP and event.key in self.key_downs:
+                self.key_downs.remove(event.key)
 
 
 class Vignette():
 
     def __init__(self, screen):
+        self.screen = screen
         black = (0, 0, 0)
         self.vignette_layer = pygame.Surface(screen.get_size(),
                                              pygame.SRCALPHA)
         self.vignette_layer.fill(black)
         circle_gradient = self.circle_gradient(screen.get_size())
-        circle_center = (int(screen.get_width() / 2), int(screen.get_height() / 2))
+        circle_center = (int(screen.get_width() / 2),
+                         int(screen.get_height() / 2))
         scale_factor = 1.0
-        circle_gradient = pygame.transform.rotozoom(circle_gradient, 0, scale_factor)
+        circle_gradient = pygame.transform.rotozoom(circle_gradient, 0,
+                                                    scale_factor)
         blit_pos = (int(circle_center[0] - circle_gradient.get_width() / 2),
                     int(circle_center[1] - circle_gradient.get_height() / 2))
-        self.vignette_layer.blit(circle_gradient, blit_pos,
+        self.vignette_layer.blit(circle_gradient,
+                                 blit_pos,
                                  special_flags=pygame.BLEND_RGBA_MULT)
 
     def linear_gradient(self, size):
@@ -105,5 +119,5 @@ class Vignette():
             pygame.draw.ellipse(alpha_gradient, (0, 0, 0, alpha), rect)
         return alpha_gradient
 
-    def draw(self, screen):
-        screen.blit(self.vignette_layer, (0, 0))
+    def draw(self):
+        self.screen.blit(self.vignette_layer, (0, 0))
